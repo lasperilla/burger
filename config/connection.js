068c7cda947f1d6f78243b7084330f9eba820d61
lasperilla <l.asperilla@gmail.com>
2017-05-16 14:53:30 -0400

    reverting connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,14 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+const pass = require("./pw.js")
+
+const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: pass.pass,
+    database: "burgers_db"
+});
 
 // Make connection.
 connection.connect(function(err) {
