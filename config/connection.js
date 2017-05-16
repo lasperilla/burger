456afdb75b499cb744c52d6fc0ab33949b5905ea
lasperilla <l.asperilla@gmail.com>
2017-05-16 14:37:15 -0400

    following jaws db instructions

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,6 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
-const pass = require("./pw.js")
-
-const connection = mysql.createConnection({
-    host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    password: pass.pass,
-    database: "burgers_db"
-});
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 // Make connection.
 connection.connect(function(err) {
