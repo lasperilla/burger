8708d4d69274ddf9271dc55aca6881efb13727d2
lasperilla <l.asperilla@gmail.com>
2017-05-18 09:00:18 -0400

    fixing connection for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 const pass = require("./pw.js")
+var connection;
 
-const connection = mysql.createConnection({
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
     host: "127.0.0.1",
     port: 3306,
     user: "root",
     password: pass.pass,
     database: "burgers_db"
-});
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
