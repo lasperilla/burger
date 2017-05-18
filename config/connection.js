2f938239776d05ecb5d6c946283a4f0620b0742e
lasperilla <l.asperilla@gmail.com>
2017-05-18 09:23:34 -0400

    debugging connection

--- config/connection.js
+++ config/connection.js
@@ -6,13 +6,14 @@ var connection;
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
-  connection = mysql.createConnection({
-    host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    password: pass.pass,
-    database: "burgers_db"
-  });
+  // connection = mysql.createConnection({
+  //   host: "127.0.0.1",
+  //   port: 3306,
+  //   user: "root",
+  //   password: pass.pass,
+  //   database: "burgers_db"
+  // });
+  console.log('didnt connect to jaws')
 };
 
 // Make connection.
